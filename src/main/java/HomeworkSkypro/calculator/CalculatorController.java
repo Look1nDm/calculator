6069e2ee3b4cc3e78fefed96b49c10afacc3d45d
dimka <dimka411841994@gmail.com>
2022-08-25 20:35:39 +0300

    подправил замечания

--- src/main/java/HomeworkSkypro/calculator/CalculatorController.java
+++ src/main/java/HomeworkSkypro/calculator/CalculatorController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
@@ -21,24 +23,36 @@ public class CalculatorController {
 
     @GetMapping(path = "/plus")
     public String plus(@RequestParam double num1, @RequestParam double num2) {
+        if (Objects.isNull(num1)||Objects.isNull(num2)){
+            return "Введите оба числа";
+        }
         double sum = calculatorService.plus(num1,num2);
         return num1+" + "+ num2+" = "+sum;
     }
 
     @GetMapping (path = "/minus")
     public String minus(@RequestParam double num1,@RequestParam double num2) {
+        if (Objects.isNull(num1)||Objects.isNull(num2)){
+            return "Введите оба числа";
+        }
         double result = calculatorService.minus(num1, num2);
         return num1+" - "+ num2+" = "+result;
     }
 
     @GetMapping (path = "/multiply")
     public String multiply(@RequestParam double num1,@RequestParam double num2) {
+        if (Objects.isNull(num1)||Objects.isNull(num2)){
+            return "Введите оба числа";
+        }
         double result = calculatorService.multiply(num1,num2);
         return num1+" * "+ num2+" = "+result;
     }
 
     @GetMapping (path = "/divide")
     public String divide(@RequestParam double num1,@RequestParam double num2) {
+        if (Objects.isNull(num1)||Objects.isNull(num2)){
+            return "Введите оба числа";
+        }
         if (num2 == 0){
             return "На 0 делить нельзя!";
         }
