c952d18ff09814ff6475f9a16924db459d600bad
dimka <dimka411841994@gmail.com>
2022-08-25 17:53:28 +0300

    first commit доработал деление на 0

--- src/main/java/HomeworkSkypro/calculator/CalculatorServiceImpl.java
+++ src/main/java/HomeworkSkypro/calculator/CalculatorServiceImpl.java
@@ -1,5 +1,8 @@
 package HomeworkSkypro.calculator;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorServiceImpl implements CalculatorService {
     public String hello() {
         return "Добро пожаловать в калькулятор!";
@@ -18,6 +21,12 @@ public class CalculatorServiceImpl implements CalculatorService {
     }
 
     public String divide(int num1, int num2) {
-        return num1 + " / " + num2 + " = " + (num1 / num2);
+        try {
+            int div = num1 / num2;
+        } catch (ArithmeticException e){
+            return "Делить на 0 нельзя!";
+        }
+            return num1 + " / " + num2 + " = " + (num1 / num2);
+
     }
 }
